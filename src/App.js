ada554b6664603dbe05c8401bc4c9348671e3dd3
HriteshSahoo2004 <ihriteshsahoo@gmail.com>
2024-07-31 19:39:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import {BrowserRouter , Routes, Route} from "react-router-dom";
 import './App.css';
+import LoginPage from "./pages/LoginPage";
+import Signup from "./pages/SignupPage";
+import ContactUsPage from "./pages/ContactUsPage";
+import ErrorPage from "./pages/ErrorPage";
+import SessionPage from "./pages/SessionPage";
+import ProfilePage from "./pages/ProfilePage";
+import UserHomeContent from "./components/UserHomeContent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<LoginPage />} />
+          <Route path = "/signin" element = {<Signup />} />
+          <Route path = "/contact" element = {<ContactUsPage />} />
+          <Route path = "/errorwhilelogin" element = {<ErrorPage />} />
+          <Route path = "/sessionexpired" element = {<SessionPage />} />
+          <Route path = "/profile" element = {<ProfilePage />} />
+          <Route path = "/userhome" element = {<UserHomeContent />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
